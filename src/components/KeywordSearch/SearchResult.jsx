1f2b4921ec455f92c3833792abb6159401c9749a
Yisen <yisenliu@gmail.com>
2023-08-14 11:36:57 +0800

    highlight matched keyword and NavItem style

--- src/components/KeywordSearch/SearchResult.jsx
+++ src/components/KeywordSearch/SearchResult.jsx
@@ -5,28 +5,29 @@ import highlightWords from 'highlight-words';
 import IconButton from '@mui/material/IconButton';
 import StockContext from '@contexts/StockContext';
 
+function HighlightWords({ text, query }) {
+  const chunks = highlightWords({
+    text,
+    query,
+    matchExactly: true,
+  });
+  return chunks.map(({ text, match, key }) =>
+    match ? (
+      <span className="text-amber-500" key={key}>
+        {text}
+      </span>
+    ) : (
+      <span key={key}>{text}</span>
+    ),
+  );
+}
+
 function Stock({ keyword, stock }) {
   const { dispatch, watchList, market } = useContext(StockContext);
   const stockList = watchList[market];
   const { stock_id, stock_name, industry_category, type } = stock;
   const isExist = stockList.some(stock => stock.id === stock_id);
 
-  function createHighlightWordsChunks(text, query) {
-    const chunks = highlightWords({
-      text,
-      query,
-      matchExactly: true,
-    });
-    return chunks.map(({ text, match, key }) =>
-      match ? (
-        <span className="text-amber-500" key={key}>
-          {text}
-        </span>
-      ) : (
-        <span key={key}>{text}</span>
-      ),
-    );
-  }
   function addToWatchList() {
     dispatch({ type: 'add_stocks', market, stocks: [{ id: stock_id }] });
   }
@@ -37,13 +38,11 @@ function Stock({ keyword, stock }) {
 
   return (
     <div className="flex items-center justify-between p-2 space-x-4">
-      {/* <p className="w-16 text-lg">
-        <span className="text-amber-500">{stock_id.slice(0, keyword.length)}</span>
-        {stock_id.slice(keyword.length, stock_id.length)}
-      </p> */}
-      <p className="w-16 text-lg">{createHighlightWordsChunks(stock_id, keyword)}</p>
+      <p className="w-16 text-lg">
+        <HighlightWords text={stock_id} query={keyword} />
+      </p>
       <p className="flex-1">
-        {createHighlightWordsChunks(stock_name, keyword)}
+        <HighlightWords text={stock_name} query={keyword} />
         {market === 'tw' && (
           <span className="block text-sm text-gray-500">
             {industry_category}-{type.toUpperCase()}
