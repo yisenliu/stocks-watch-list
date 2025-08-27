edaec1148c5a41630f8509cfaa9a0675dbaa6517
Yisen <yisenliu@gmail.com>
2025-08-27 10:02:20 +0800

    add 'Speed Insights'

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -3,10 +3,12 @@ import '@lib/pace';
 import Container from './Container';
 import Header from './Header';
 import Main from './Main';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function Layout() {
   return (
     <Container>
+      <SpeedInsights />
       <Header />
       <Main>
         <Outlet />
