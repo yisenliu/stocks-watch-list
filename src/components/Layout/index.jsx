456e40e727104d0a3d83a49bef19530ee5e9e96e
Yisen <yisenliu@gmail.com>
2025-08-27 10:22:50 +0800

    modify SpeedInsights path

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -3,16 +3,16 @@ import '@lib/pace';
 import Container from './Container';
 import Header from './Header';
 import Main from './Main';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 export default function Layout() {
   return (
     <Container>
-      <SpeedInsights />
       <Header />
       <Main>
         <Outlet />
       </Main>
+      <SpeedInsights />
     </Container>
   );
 }
